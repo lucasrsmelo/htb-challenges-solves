b6366eb72a113d10582dd28a4269bb3a196a81d0
Melo-Lucas <lucs.r06@gmail.com>
2021-04-30 13:48:48 -0300

    Feat: trata 'IndexError' e 'JSONDecodeError'. Extrai apenas dados relevantes do html retornado

--- Web/Emdee_five_for_life/test.py
+++ Web/Emdee_five_for_life/test.py
@@ -4,12 +4,30 @@ import sys
 import requests
 import hashlib
 import re
+import json
+from json.decoder import JSONDecodeError
 
 try:
+    # lê parâmetro direto da chamada do arquivo .py
+    # nesse caso, o esperado é: test.py <url>
+    # EXEMPLO: test.py https://www.pudim.com.br 
 
-    str(sys.argv[1])
-    print(f"The website you prompted is: {sys.argv[1]}")
+    url = sys.argv[1]
+
+    resp = requests.get(url)
+
+    html_content = resp.content
+
+    def html_cleaning(html):
+        cleaning = re.compile('<.*?>')
+        return re.sub(cleaning, '', html)
+
+    print(html_cleaning(str(html_content)))
 
 except IndexError:
 
     print("Usage: test.py <target_url>")
+    print("Usage example: test.py https://www.pudim.com.br")
+
+except JSONDecodeError:
+    print("ERROR: Decoding JSON has failed")
