94c4b311a0d27cc74446808abd8ceb51cc7354f9
Melo-Lucas <lucs.r06@gmail.com>
2021-05-26 04:44:12 -0700

    Finally the script works and returned me the flag

--- Web/Emdee_five_for_life/test2.py
+++ Web/Emdee_five_for_life/test2.py
@@ -15,5 +15,8 @@ html = get_req.content
 soup = BeautifulSoup(html, 'html.parser')
 hash_val = soup.h3.get_text().encode('utf-8')
 
-print(hash_val)
+md5hash = hashlib.md5(hash_val).hexdigest()
+data = dict(hash=md5hash)
+post_res = req.post(url = url, data = data)
+print("flag: ", post_res.text)
 
